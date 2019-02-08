d33e5ccc97dd31eed82e4581169c4171db14bd82
Hunter Kreshock <hkreshock@gmail.com>
2019-02-08 09:32:52 -0700

    added features.js and header.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header'
+import Features from './components/Features'
 
 class App extends Component {
   constructor(props){
@@ -48,42 +50,13 @@ class App extends Component {
     const total = Object.keys(this.state.selected)
           .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
 
-
-    const features = Object.keys(this.props.features)
-          .map(key => {
-            const options = this.props.features[key].map((item, index) => {
-              const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  
-                  onClick={e => this.updateFeature(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });      
-
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing</h1>
-          <h3>Laptops</h3>
-          <h5>Customize your laptop</h5>  
-        </header>      
+      <Header></Header> 
         <main>
           <section className="main__form">
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            { features }
+             <Features features= {this.props.features} selected= {this.state.selected} onClick={(feature, newValue) => this.updateFeature(feature, newValue)}></Features>
           </section>
           <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
@@ -102,4 +75,4 @@ class App extends Component {
   }
 }
 
-export default App;  
+export default App;
